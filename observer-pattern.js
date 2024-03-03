08338b8485c24994fc1f4906e09bf9e925d10999
derajsp <DeraJSP@gmail.com>
2024-03-03 23:33:22 +0100

    added phone no

--- observer-pattern.js
+++ observer-pattern.js
@@ -5,33 +5,11 @@ class telephone {
 		this.observers = [];
 		// empty array to hold contacts stored
 		this.contacts = [];
-		// hold the number dialed
+		// store the number dialed
 		this.dialedNumber;
 	}
 
-	// add observer to observer array/list
-	addObserver(observer) {
-		this.observers.push(observer);
-	}
-
-	// remove observer from array/list
-	removeObserver(observer) {
-		this.observers = this.observers.filter(obs => obs !== observer);
-	}
-
-	// method to notify observers
-	notifyObservers() {
-		const observer1 = this.observers[0];
-    	if (observer1) {
-      		observer1.update(this.dialedNumber);
-   		}
-	    const observer2 = this.observers[1];
-	    	if (observer2) {
-	      		observer2.update(`Now dialling ${this.dialedNumber}`);
-		}
-  	}
-
-  	// add a number to contacts array
+	// add a number to contacts array
 	addPhoneNumber(number) {
 		this.contacts.push(number);
 	}
@@ -52,12 +30,35 @@ class telephone {
 		    	console.log('Phone number not found!');
 		  }
 	}
+
+	// add observer to observer array/list
+	addObserver(observer) {
+		this.observers.push(observer);
+	}
+
+	// remove observer from array/list
+	removeObserver(observer) {
+		this.observers = this.observers.filter(obs => obs !== observer);
+	}
+
+	// method to notify observers
+	notifyObservers() {
+		// first observer
+		const observer1 = this.observers[0];
+    	if (observer1) {
+      		observer1.update(this.dialedNumber);
+   		}
+   		// second observer
+	    const observer2 = this.observers[1];
+	    	if (observer2) {
+	      		observer2.update(`Now dialling ${this.dialedNumber}`);
+		}
+  	}
 } 
 
 // define an observer class
 class myObservers {
 	constructor(name) {
-		// store observer name
 		this.name = name;
 	}
 
@@ -70,20 +71,15 @@ class myObservers {
 const android = new telephone();
 
 // two instances of the observer class
-const displayDevice1 = new myObservers("First observer");
-const displayDevice2 = new myObservers("Second observer");
+const displayDevice1 = new myObservers("Display device 1");
+const displayDevice2 = new myObservers("Display device 2");
 
 // add both display devices as observers to the android subject
 android.addObserver(displayDevice1);
 android.addObserver(displayDevice2);
 
 // add phone number to the contacts array
-android.addPhoneNumber(23480123);
-android.addPhoneNumber(23480456);
-android.addPhoneNumber(23480789);
-android.addPhoneNumber(23470123);
-android.addPhoneNumber(23470456);
-android.addPhoneNumber(23470789);
+android.addPhoneNumber(2347023232);
 
 // dial phone number
-android.dialPhoneNumber(23480123);
+android.dialPhoneNumber(2347023232);
